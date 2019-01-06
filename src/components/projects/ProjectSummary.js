6a447f4028bbbc3737273a541373b450a97b4236
danchamorro <dan@danchamorro.com>
2019-01-06 03:17:04 -0500

    Added dummy data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="project-list section">
       <div className="card z-depth-0 project-summary">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{project.title}</span>
         <p>Posted By Dan the Man</p>
         <p className="grey-text">6th January, 2am</p>
       </div>
