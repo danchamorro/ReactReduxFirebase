720f8f0df5f0d05a55f45fa26a6e8bb7f9e35e5f
danchamorro <dan@danchamorro.com>
2019-01-06 00:30:08 -0500

    Added Project List, Dashboard and Notifictions. Also Dashboard route.

---
+++ src/components/dashboard/Notfications.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notfications = () => {
+  return (
+    <div>
+      <p>Notification</p>
+    </div>
+  );
+};
+
+export default Notfications;
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div>
+      <div className="project-list section">
+        <div className="card z-depth-0 project-summary">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Dan the Man</p>
+          <p className="grey-text">6th January, 2am</p>
+        </div>
+      </div>
+
+      <div className="project-list section">
+        <div className="card z-depth-0 project-summary">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Dan the Man</p>
+          <p className="grey-text">6th January, 2am</p>
+        </div>
+      </div>
+
+      <div className="project-list section">
+        <div className="card z-depth-0 project-summary">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Dan the Man</p>
+          <p className="grey-text">6th January, 2am</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
