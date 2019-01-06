f253f58592442f8fe66c648e78ad5dbb8da94a6f
danchamorro <dan@danchamorro.com>
2019-01-06 00:51:12 -0500

    Refacotred Roject List tp be dynamic

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
   return (
-    <div>
-      <div className="project-list section">
-        <div className="card z-depth-0 project-summary">
-          <span className="card-title">Project Title</span>
-          <p>Posted By Dan the Man</p>
-          <p className="grey-text">6th January, 2am</p>
-        </div>
-      </div>
-
-      <div className="project-list section">
-        <div className="card z-depth-0 project-summary">
-          <span className="card-title">Project Title</span>
-          <p>Posted By Dan the Man</p>
-          <p className="grey-text">6th January, 2am</p>
-        </div>
-      </div>
-
-      <div className="project-list section">
-        <div className="card z-depth-0 project-summary">
-          <span className="card-title">Project Title</span>
-          <p>Posted By Dan the Man</p>
-          <p className="grey-text">6th January, 2am</p>
-        </div>
-      </div>
+    <div className="project-list section">
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <span className="card-title">Project Title</span>
+        <p>Posted By Dan the Man</p>
+        <p className="grey-text">6th January, 2am</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
